1792896127ba2503b8d24eaba8e35c68c8d4ad00
EscapeFromJava <reske.artur@gmail.com>
2022-10-03 22:52:15 +0300

    Project skeleton
    add main classes

---
+++ src/main/java/com/example/instazoo/entity/User.java
@@ -0,0 +1,51 @@
+package com.example.instazoo.entity;
+
+import com.example.instazoo.entity.enums.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Data
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(unique = true)
+    private String username;
+    @Column(length = 3000)
+    private String password;
+    @Column(unique = true)
+    private String email;
+    @Column(columnDefinition = "text")
+    private String bio;
+
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"))
+    private Set<ERole> roles = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
+}
